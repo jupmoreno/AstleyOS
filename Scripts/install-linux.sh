95fa8671f0416d01823ef0545568fa54d75a11fb
Juan <jpmrno@gmail.com>
2016-05-25 17:04:25 -0300

    Linux installation fix

--- Scripts/install-linux.sh
+++ Scripts/install-linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -ev
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get update -qq
 sudo apt-get install make gcc-5 build-essential nasm qemu -y

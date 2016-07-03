44612699a2ef39c7e1416449c5fb6228ccec7b85
natalia navas <natinavas@gmail.com>
2016-07-03 16:45:15 -0300

    muscia continua que suena espantosa

--- Userland/shell/src/commands/game.c
+++ Userland/shell/src/commands/game.c
@@ -15,13 +15,14 @@ extern void sys_sound(int frequency, double time);
 extern int sys_new_process(const char* name, uint64_t func, uint64_t argc, void* argv);
 extern void sys_send_message(uint64_t sender, uint64_t receiver, uint64_t size, void* message);
 extern read_msg sys_read_message(uint64_t pid);
-
+void playRick(void);
 
 COMMAND_FUNCTION(game);
 COMMAND_HELP(game);
 
 COMMAND_FUNCTION(game) {
 	sysvideo_mode();
+	//playRick();
 	
 	uint64_t game_pid = sys_new_process("snake", (uint64_t) &startGame, 0, 0);
 	uint64_t control_pid = sys_new_process("snake_input", (uint64_t) &snake_input, 0, 0);
@@ -29,7 +30,8 @@ COMMAND_FUNCTION(game) {
 	//le mando el pid del juego al proceso que controla las teclas
 	sys_send_message(game_pid, control_pid, 1, (void*)game_pid);
 	
-	//sys_new_process("game_music", (uint64_t)&play_song, 1, (void*)RICK);
+	
+	//sys_new_process("game_music", (uint64_t) &playRick, 0, 0);
 
 	while(1); // TODO: REMOVE
 
@@ -54,5 +56,7 @@ void paintPixel(int x, int y, int blue_c, int green_c, int red_c){
 }
 
 void playRick(){
-	play_song(RICK);
+	while(1){
+		play_song(RICK);
+	}
 }
